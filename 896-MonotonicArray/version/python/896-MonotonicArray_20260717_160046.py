4ad6c332fa3ff5656a38f079f1b92ad8fe7a03a3
Maisam A. <45243399+Koorikdat@users.noreply.github.com>
2026-07-17 16:00:46 -0400

    Create 896-MonotonicArray

---
+++ 896-MonotonicArray/version/python/896-MonotonicArray_20260717_160046.py
@@ -0,0 +1,14 @@
+# Last updated: 7/17/2026, 4:00:46 PM
+1class Solution:
+2    def isMonotonic(self, nums: List[int]) -> bool:
+3        increasing = True
+4        decreasing = True
+5
+6        for i in range(len(nums) - 1):
+7            if nums[i] > nums[i + 1]:
+8                increasing = False
+9            if nums[i] < nums[i + 1]:
+10                decreasing = False
+11
+12        return increasing or decreasing
+13
