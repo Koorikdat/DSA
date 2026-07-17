2f102c1f891b747aeefc935eb164044660e4bfc1
Maisam A. <45243399+Koorikdat@users.noreply.github.com>
2026-07-17 11:57:53 -0400

    Create 1001-NRepeatedElementInSize2nArray

---
+++ 1001-NRepeatedElementInSize2nArray/1001-NRepeatedElementInSize2nArray.py
@@ -0,0 +1,17 @@
+# Last updated: 7/17/2026, 11:57:53 AM
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+
+
+        n = len(nums) // 2
+
+        seen = {}
+        for i in range(0, len(nums)):
+            if nums[i] in seen:
+                seen[nums[i]] += 1
+            else:
+                seen[nums[i]] = 1  
+
+        for element, frequency in seen.items(): 
+            if frequency == n: 
+                return element
