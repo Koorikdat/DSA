074a4697527d57d53b52e3c770d07232b654393c
Maisam A. <45243399+Koorikdat@users.noreply.github.com>
2026-07-23 11:11:54 -0400

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/version/python/69-Sqrt(x)_20260723_111153.py
@@ -0,0 +1,14 @@
+# Last updated: 7/23/2026, 11:11:53 AM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3
+4        seen = {}
+5
+6        for i in range (0, len(nums)):
+7            goal = target - nums[i]
+8            if goal in seen:
+9                return [i, seen[goal]]
+10            else:
+11                seen[nums[i]] = i
+12
+13
