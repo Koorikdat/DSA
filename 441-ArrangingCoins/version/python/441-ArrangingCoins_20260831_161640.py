232eb8b5d223256632f88213a7804d8d398ebf10
Maisam A. <45243399+Koorikdat@users.noreply.github.com>
2026-08-31 15:16:41 -0500

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/version/python/441-ArrangingCoins_20260831_161640.py
@@ -0,0 +1,27 @@
+# Last updated: 8/31/2026, 4:16:40 PM
+1class Solution:
+2    def checkRecord(self, s: str) -> bool:
+3
+4        ACounter = 0
+5        LCounter = 0
+6
+7        for i in range (0, len(s)):           
+8
+9            if s[i] == "A":
+10                ACounter += 1
+11                LCounter = 0
+12            
+13            if s[i] == "L":
+14                LCounter += 1
+15            
+16            if s[i] == "P":
+17                LCounter = 0
+18
+19            if ACounter==2 or LCounter==3:
+20                return False
+21        
+22        return True
+23 
+24
+25            
+26        
